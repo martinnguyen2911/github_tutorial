6bf4819081e17dde54ef2f484a62061a5df835e5
Martin Nguyen <martin.nguyen2911@gmail.com>
2025-06-26 18:18:04 -0700

    mean branch

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,18 @@
 
-print("Horld!")
-print("goodbye, World!")
+print("Hello, World!")
+def mean(input):
+  sum = 0
+  for entry in input:
+    sum += entry
+  return sum / len(input)
+
+my_list = [0, 1, 2, 3, 4, 5]
+print(mean(my_list))
+
+import math
+def median(input):
+  mid = len(input)//2
+  return (input[mid-1] + input[mid])/2
+
+my_list = [0, 1, 2, 3, 4, 5]
+print(median(my_list))

cca2f188cd704fe52696e8138ba03008121f4b21
Martin Nguyen <martin.nguyen2911@gmail.com>
2025-06-26 18:24:37 -0700

    mean_branch

--- hello_world.py
+++ hello_world.py
@@ -8,11 +8,3 @@ def mean(input):
 
 my_list = [0, 1, 2, 3, 4, 5]
 print(mean(my_list))
-
-import math
-def median(input):
-  mid = len(input)//2
-  return (input[mid-1] + input[mid])/2
-
-my_list = [0, 1, 2, 3, 4, 5]
-print(median(my_list))

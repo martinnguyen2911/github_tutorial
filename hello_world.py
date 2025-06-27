e1dc8d2440b3365cae7025c091186706e0d6ba19
Martin Nguyen <martin.nguyen2911@gmail.com>
2025-06-26 18:25:42 -0700

    median_branch

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,8 @@
 
-print("Horld!")
-print("goodbye, World!")
+import math
+def median(input):
+  mid = len(input)//2
+  return (input[mid-1] + input[mid])/2
+
+my_list = [0, 1, 2, 3, 4, 5]
+print(median(my_list))

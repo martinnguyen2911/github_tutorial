a3626d17f041e8f9a9204b098ed92d899d2620ef
martinnguyen2911 <martin.nguyen2911@gmail.com>
2025-06-27 19:05:40 -0700

    mean_branch

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,3 @@
-print("hello_world")
-
-import math
-def median(input):
-  mid = len(input)//2
-  return (input[mid-1] + input[mid])/2
-
-my_list = [0, 1, 2, 3, 4, 5]
-print(median(my_list))
-
-
 def mean(input):
   sum = 0
   for entry in input:

08ee2c46604ebdeb448076fc21d9161a1fb1c5b5
Martin Nguyen <martin.nguyen2911@gmail.com>
2025-06-27 18:01:18 -0700

    reset

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
-
+print("hello_world")
 
 import math
 def median(input):
@@ -8,12 +8,4 @@ def median(input):
 my_list = [0, 1, 2, 3, 4, 5]
 print(median(my_list))
 
-print("Hello, World!")
-def mean(input):
-  sum = 0
-  for entry in input:
-    sum += entry
-  return sum / len(input)
 
-my_list = [0, 1, 2, 3, 4, 5]
-print(mean(my_list))
